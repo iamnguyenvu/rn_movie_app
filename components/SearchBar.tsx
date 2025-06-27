19269f1a4bbffe8bb757438159be333efdbb9617
iamnguyenvu <iamnguyenvu.gm@gmail.com>
2025-06-27 09:39:21 +0700

    initialized project, routing & navigation, customized app title & icon

---
+++ components/SearchBar.tsx
@@ -0,0 +1,22 @@
+import {Image, Text, TextInput, View} from "react-native";
+import {icons} from "@/constants/icons";
+
+const SearchBar = () => {
+    return (
+        <View className="flex-row items-center px-5 py-4 bg-dark-200 rounded-full">
+            <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff"/>
+            <TextInput
+                onPress={() => {
+                }}
+                placeholder="Search"
+                value=""
+                onChangeText={() => {
+                }}
+                placeholderTextColor="#ab8bff"
+                className="flex-1 ml-2 text-white"
+            />
+        </View>
+    )
+}
+
+export default SearchBar;
